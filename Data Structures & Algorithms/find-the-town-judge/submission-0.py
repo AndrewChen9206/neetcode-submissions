3200d951bac6949085cf452315fd466237a23da8
Yogurt Chen <andrew.yongsyuan.chen@gmail.com>
2026-06-21 10:09:58 +0800

    Add: find-the-town-judge - submission-0

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-0.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findJudge(self, n, trust):
+        """
+        :type n: int
+        :type trust: List[List[int]]
+        :rtype: int
+        """
+        if n == 1 and not trust:
+            return 1
+
+        in_degree = defaultdict(int)
+        out_degree = defaultdict(int)
+
+        for a, b in trust:
+            out_degree[a] += 1
+            in_degree[b] += 1
+
+        for person in range(1, n + 1):
+            if in_degree[person] == n - 1 and out_degree[person] == 0:
+                return person
+
+        return -1
