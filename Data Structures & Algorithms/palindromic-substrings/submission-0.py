ba0d8deb9c32420a51ecf46786babcc4484270cb
Yogurt Chen <andrew.yongsyuan.chen@gmail.com>
2026-06-22 11:10:59 +0800

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res = 0
+        dp = [[False] * len(s) for _ in range(len(s))]
+
+        for i in range(len(s)-1, -1, -1):
+            for j in range(i, len(s)):
+                dp[i][j] = (s[i] == s[j]) and ((j - i < 3) or (dp[i+1][j-1]))
+                if dp[i][j]:
+                    res += 1
+        
+        return res
