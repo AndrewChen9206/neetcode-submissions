7aee0d45a01b91ca2f09be093ff79de5483e814e
Yogurt Chen <andrew.yongsyuan.chen@gmail.com>
2026-06-28 16:11:27 +0800

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = len(height) - 1
+        leftMax = height[left]
+        rightMax = height[right]
+        water = 0
+
+        while left < right:
+            if leftMax < rightMax:
+                water += leftMax - height[left]
+                left += 1
+                leftMax = max(leftMax, height[left])
+            else:
+                water += rightMax - height[right]
+                right -= 1
+                rightMax = max(rightMax, height[right])
+        
+        return water
