335d690b669cf586ac41b35865a32514bcdefb51
Yogurt Chen <andrew.yongsyuan.chen@gmail.com>
2026-07-11 11:01:12 +0800

    Add: interleaving-string - submission-0

---
+++ Data Structures & Algorithms/interleaving-string/submission-0.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def isInterleave(self, s1, s2, s3):
+        """
+        :type s1: str
+        :type s2: str
+        :type s3: str
+        :rtype: bool
+        """
+        if len(s1) + len(s2) != len(s3):
+            return False
+        
+        dp = [[False] * (len(s2)+1) for _ in range(len(s1)+1)]
+        dp[0][0] = True
+
+        for i in range(1, len(s1)+1):
+            if s1[i-1] != s3[i-1]:
+                break
+
+            dp[i][0] = True
+
+        for j in range(1, len(s2)+1):
+            if s2[j-1] != s3[j-1]:
+                break
+                
+            dp[0][j] = True
+        
+        for i in range(1, len(s1)+1):
+            for j in range(1, len(s2)+1):
+                dp[i][j] = dp[i-1][j] and s1[i-1] == s3[i+j-1]
+                dp[i][j] = dp[i][j] or (dp[i][j-1] and s2[j-1] == s3[i+j-1])
+        
+        return dp[len(s1)][len(s2)]
